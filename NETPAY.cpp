59040a74a083cb87b139c813c85eff51d6becc95
Steffano99 <101764484+Steffano99@users.noreply.github.com>
2023-07-21 09:52:39 +0000

    Update NETPAY.cpp

--- NETPAY.cpp
+++ NETPAY.cpp
@@ -1,8 +1,11 @@
+//this C++ program is open to contributions and editing.
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
 int main() {
+	//declaration of variables that will hold the various parameters of the program
+	
     float workedHours, overtime, workedIncome, overtimeIncome, grossPay = 0.0, regularHours, socialSecurity=0.06, constituencyTax=0.03,deduction, welfare=0.02, netSalary;
     const float weeklyHours = 40;
     string name;
@@ -10,17 +13,21 @@ int main() {
 	const int dependants=3;
 	int noDependants;
 	int excessDependants;
-    
+//this section allows vrious workers to enter their username and pin before inputing other details for determination of netpay.
+	
     cout<<"Enter your username:"<<endl;
     cin>>name;
         cout<<"Enter your pin :"<<endl;
         cin>>pin;
 
-    // Taking the number of hours used for working from a user
+    // Taking the number of hours used for working from a user  and the number of dependants of a user
+	
     cout << "Enter the number of worked hours in a week: " << endl;
     cin >> workedHours;
         cout << "Enter the number of dependants: " << endl;
 cin>>noDependants;
+	// setting up a conditions to calculate the grosspay of a worker
+	
     if (workedHours <= weeklyHours) {
         grossPay = workedHours * 25000.00;
         cout << "Your grossPay is: " << fixed << setprecision(2) << grossPay << endl;
@@ -31,11 +38,10 @@ cin>>noDependants;
         grossPay = (regularHours * 25000.00) + (overtime * 1.5 * 25000.00);
         cout << "Your grossPay is: " << fixed << setprecision(2) << grossPay << endl;
     }
-    
+
+	//setting up conditions that takes into consideration the user gross pay and the various taxes involved and return the netpay of a user
     
 if (grossPay<125001){
-	
-	
 if(noDependants>3){
 		excessDependants=noDependants-dependants;
 }
@@ -44,7 +50,6 @@ if(noDependants>3){
 	float netPay=netSalary+(excessDependants*5000);
 	        cout << "Your netPay is: " << fixed << setprecision(2) << netPay << endl;
 	        	        cout << "amount deducted: " << fixed << setprecision(2) << deduction << endl;
-
 
 }
 else if(grossPay>125001&&grossPay<250000){
@@ -93,7 +98,6 @@ else if(grossPay>2750001&&grossPay<5000000){
 	float netPay=netSalary+(excessDependants*5000);
 		        cout << "Your netPay is: " << fixed << setprecision(2) << netPay << endl;
 		        	        	        cout << "amount deducted: " << fixed << setprecision(2) << deduction << endl;
-
 }
 else if(grossPay>5000000){
 	if(noDependants>3){
